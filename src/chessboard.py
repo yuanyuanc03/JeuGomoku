3c91d081752d3dca64e77ad6157383b92b4e2516
yuanyuan <yuanyuan.liu.c03@gmail.com>
2021-02-05 10:24:46 +0100

    exit button

--- src/chessboard.py
+++ src/chessboard.py
@@ -16,10 +16,10 @@ class Button:
     def change_text(self, text, bg="black"):
         self.text = self.font.render(text, 1, pygame.Color("white"))
         self.size = self.text.get_size()
-        self.surface = pygame.Surface((self.size[0] + 30, self.size[1] + 10))
+        self.surface = pygame.Surface((self.size[0] + 130, self.size[1] + 10))
         self.surface.fill(bg)
-        self.surface.blit(self.text, (15, 5))
-        self.rect = pygame.Rect(self.x, self.y, self.size[0] + 30, self.size[1] + 30)
+        self.surface.blit(self.text, (65, 5))
+        self.rect = pygame.Rect(self.x, self.y, self.size[0] + 130, self.size[1] + 130)
 
 class Chess(object):
     def __init__(self):
@@ -39,7 +39,8 @@ class Chess(object):
         # background image
         self.bg_path = "../img/background.jpg"
         # create buttons
-        self.btn_start = Button("Start", x = 250, y = 80, bg="grey")
+        self.btn_start = Button("Start", x = 200, y = 80, bg="sandybrown")
+        self.btn_exit = Button("Exit", x = 450, y = 80, bg="sandybrown")
 
     def btnStart_click(self, event):
         x, y = pygame.mouse.get_pos()
@@ -48,11 +49,19 @@ class Chess(object):
                 if self.btn_start.rect.collidepoint(x, y):
                     self.draw()
 
+    def btnExit_click(self, event):
+        x, y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                if self.btn_exit.rect.collidepoint(x, y):
+                    pygame.quit()
+
     def load_bg(self):
         while True:
             bg_img = pygame.transform.scale(pygame.image.load(self.bg_path), (self.grid_size, self.grid_size))
             self.screen.blit(bg_img, (0, 0))
             self.screen.blit(self.btn_start.surface, (self.btn_start.x, self.btn_start.y))
+            self.screen.blit(self.btn_exit.surface, (self.btn_exit.x, self.btn_exit.y))
             pygame.display.update()
 
             for event in pygame.event.get():
@@ -60,6 +69,7 @@ class Chess(object):
                     pygame.quit()
                     exit()
                 self.btnStart_click(event)
+                self.btnExit_click(event)
 
     def draw(self):
         # background color
@@ -118,92 +128,38 @@ class Chess(object):
             self.board[row][col] = WHITE
         if not self.is_win():
             pass
+        else:
+            print(self.is_win())
             # running = False
 
     def is_win(self):
-        for n in range(15):
-            flag = 0
-            for b in self.board:
-                if b[n] == 1:
-                    flag += 1
-            if flag == 5:
-                print('Black win!')
-                return False
-            else:
-                flag = 0
-
-            for b in self.board:
-                if b[n] == 2:
-                    flag += 1
-            if flag == 5:
-                print('White win!')
-                return False
-            else:
-                flag = 0
-
-            for b in self.board[n]:
-                if b == 1:
-                    flag += 1
-            if flag == 5:
-                print('Black win!')
-                return False
-            else:
-                flag = 0
-
-            for b in self.board[n]:
-                if b == 2:
-                    flag += 1
-            if flag == 5:
-                print('White win!')
-                return False
-            else:
-                flag = 0
-
-            for x in range(4, 25):
-                flag = 0
-            for i, b in enumerate(self.board):
-                if 14 >= x - i >= 0 and b[x - i] == 1:
-                    flag += 1
-                    if flag == 5:
-                        print('Black win!')
-                    return False
-                else:
-                    flag = 0
-
-            for x in range(4, 25):
-                flag = 0
-            for i, b in enumerate(self.board):
-                if 14 >= x - i >= 0 and b[x - i] == 2:
-                    flag += 1
-                    if flag == 5:
-                        print('White win!')
-                    return False
-                else:
-                    flag = 0
-
-            for x in range(11, -11, -1):
-                flag = 0
-            for i, b in enumerate(self.board):
-                if 0 <= x + i <= 14 and b[x + i] == 1:
-                    flag += 1
-                    if flag == 5:
-                        print('Black win!')
-                    return False
-                else:
-                    flag = 0
-
-            for x in range(11, -11, -1):
-                flag = 0
-            for i, b in enumerate(self.board):
-                if 0 <= x + i <= 14 and b[x + i] == 2:
-                    flag += 1
-                    if flag == 5:
-                        print('White win!')
-                    return False
-                else:
-                    flag = 0
-
-            return True
+        for c in range(self.cell_num):
+            for r in range(self.cell_num):
+                if r < self.cell_num - 4 and self.board[r][c] ==  self.board[r + 1][c] ==  self.board[r + 2][c] == \
+                        self.board[r + 3][c] == self.board[r + 4][c] == BLACK:
+                    return "Black win!"
+                elif r < self.cell_num - 4 and self.board[r][c] ==  self.board[r + 1][c] ==  self.board[r + 2][c] == \
+                        self.board[r + 3][c] == self.board[r + 4][c] == WHITE:
+                    return "White win!"
+                elif c < self.cell_num - 4 and self.board[r][c] == self.board[r][c + 1] == self.board[r][c + 2] == \
+                        self.board[r][c + 3] == self.board[r][c + 4] == BLACK:
+                    return "Black win!"
+                elif c < self.cell_num - 4 and self.board[r][c] == self.board[r][c + 1] == self.board[r][c + 2] == \
+                        self.board[r][c + 3] == self.board[r][c + 4] == WHITE:
+                    return "White win!"
+                elif r < self.cell_num - 4 and c < self.cell_num - 4 and self.board[r][c] == self.board[r + 1][c + 1] == \
+                        self.board[r + 2][c + 2] == self.board[r + 3][c + 3] == self.board[r + 4][c + 4] == BLACK:
+                    return "Black win!"
+                elif r < self.cell_num - 4 and c < self.cell_num - 4 and self.board[r][c] == self.board[r + 1][c + 1] == \
+                        self.board[r + 2][c + 2] == self.board[r + 3][c + 3] == self.board[r + 4][c + 4] == WHITE:
+                    return "White win!"
+                elif r > 3 and c < self.cell_num - 4 and self.board[r][c] == self.board[r - 1][c + 1] == \
+                        self.board[r - 2][c + 2] == self.board[r - 3][c + 3] == self.board[r - 4][c + 4] == BLACK:
+                    return "Black win!"
+                elif r > 3 and c < self.cell_num - 4 and self.board[r][c] == self.board[r - 1][c + 1] == \
+                        self.board[r - 2][c + 2] == self.board[r - 3][c + 3] == self.board[r - 4][c + 4] == WHITE:
+                    return "White win!"
+        return False
 
 if __name__ == '__main__':
     chess = Chess()
