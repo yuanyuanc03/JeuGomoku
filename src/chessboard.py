883b5abce0ae62b2193bbfa950ee6eb22f3d33ab
yuanyuan <yuanyuan.liu.c03@gmail.com>
2021-03-16 21:55:46 +0100

    add restart and back button

--- src/chessboard.py
+++ src/chessboard.py
@@ -2,6 +2,8 @@ import pygame
 import numpy as np
 
 # define global variable of pieces' color
+from pygame.tests.draw_test import GREEN
+
 EMPTY = 0
 BLACK = 1
 WHITE = 2
@@ -13,10 +15,9 @@ class Button:
         :param text: string of the button name
         :param x: position x of button
         :param y: position y of button
-        :param bg: the color of button
         """
         self.font = pygame.font.SysFont("Arial", 30)
-        self.font.set_bold(True)  # 设置是否加粗
+        self.font.set_bold(True)
         self.font.set_italic(True)
         self.x = x
         self.y = y
@@ -25,6 +26,11 @@ class Button:
         self.HEIGHT = self.surface.get_height()
 
     def check_click(self, position):
+        """
+        judge if user click the button
+        :param position:
+        :return:
+        """
         x_match = position[0] > self.x and position[0] < self.x + self.WIDTH
         y_match = position[1] > self.y and position[1] < self.y + self.HEIGHT
 
@@ -68,6 +74,17 @@ class Chess(object):
         self.btn_start = Button("Start", x = 200, y = 80)
         self.btn_startWithTimer = Button("Start with Timer", x = 320, y = 80)
         self.btn_exit = Button("Exit", x = 580, y = 80)
+        self.btn_restart = Button("Restart", x = 400, y = 15)
+        self.btn_back = Button("Back", x = 540, y = 15)
+
+        ### set timer
+        self.counts = 10
+        self.COUNT = pygame.USEREVENT + 1
+        pygame.time.set_timer(self.COUNT, 1000)
+        self.text = "Timer"
+
+        # if gameover
+        self.is_gameOver = False
 
     def btnStart_click(self, event):
         """
@@ -89,7 +106,7 @@ class Chess(object):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if pygame.mouse.get_pressed()[0]:
                 if self.btn_startWithTimer.check_click([x, y]):
-                    self.draw()
+                    self.draw(True)
 
     def btnExit_click(self, event):
         """
@@ -100,7 +117,56 @@ class Chess(object):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if pygame.mouse.get_pressed()[0]:
                 if self.btn_exit.check_click([x, y]):
-                    pygame.quit()
+                   exit()
+
+    def btnRestart_click(self, event, timer):
+        """
+        restart the game
+        :param event: if users click the button Restart then event occurs
+        """
+        x, y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                if self.btn_restart.check_click([x, y]):
+                    self.position = np.zeros((self.cell_num, self.cell_num, 2))
+                    self.board = np.zeros((self.cell_num, self.cell_num))
+                    self.chess_color = "black"
+                    self.is_black = True
+                    self.is_gameOver = False
+                    if (timer):
+                        self.counts = 10
+                    self.draw(timer)
+
+    def btnBack_click(self, event):
+        """
+        back to the main page
+        :param event: if users click the button Back then event occurs
+        """
+        x, y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                if self.btn_back.check_click([x, y]):
+                    self.position = np.zeros((self.cell_num, self.cell_num, 2))
+                    self.board = np.zeros((self.cell_num, self.cell_num))
+                    self.chess_color = "black"
+                    self.is_black = True
+                    self.is_gameOver = False
+                    self.load_bg()
+
+    def showText(self, text, x, y):
+        """
+        show text in the screen
+        :param text: string of content of text
+        :param x: the position x
+        :param y: the position y
+        """
+        text_font = pygame.font.SysFont("Arial", 25)
+        text_font.set_bold(True)
+        text_font.set_italic(True)
+        self.text = text_font.render(text, True, "sandybrown", "peachpuff")
+        self.screen.blit(self.text, (x, y))
+        # self.screen.blit(self.screen, (0, 0))
+        pygame.display.update()
 
     def load_bg(self):
         """
@@ -116,18 +182,19 @@ class Chess(object):
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
                     exit()
                 self.btnStart_click(event)
                 self.btnStartWithTimer_click(event)
                 self.btnExit_click(event)
 
-    def draw(self):
+    def draw(self, timer=False):
         """
         create the chessboard and draw the chess
         """
         # background color
         self.screen.fill("peachpuff")
+        self.screen.blit(self.btn_restart.surface, (self.btn_restart.x, self.btn_restart.y))
+        self.screen.blit(self.btn_back.surface, (self.btn_back.x, self.btn_back.y))
 
         for row in range(0, self.cell_num):
             for col in range(0, self.cell_num):
@@ -156,20 +223,38 @@ class Chess(object):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
                     exit()
                 elif event.type == pygame.K_q:
-                    pygame.quit()
                     exit()
-                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                elif timer and event.type == self.COUNT and not self.is_gameOver:
+                    self.counts = self.counts - 1
+                    counts_text = str(self.counts)
+                    self.showText("Time: " + counts_text, 260, 20)
+                    if self.counts == 0:
+                        self.gameOver("Time is out!")
+
+                elif event.type == pygame.MOUSEBUTTONDOWN and not self.is_gameOver:
                     x, y = event.pos
                     row = round((x - chess.space) / chess.cell_size)
                     col = round((y - chess.space) / chess.cell_size)
-                    if(self.board[row][col] == EMPTY):
-                        pygame.draw.circle(self.screen, self.chess_color, (self.position[row][col][0], self.position[row][col][1]), 18)
+                    if (self.board[row][col] == EMPTY):
+                        pygame.draw.circle(self.screen, self.chess_color,
+                                           (self.position[row][col][0], self.position[row][col][1]), 18)
                         self.play(row, col)
                         self.screen.blit(self.screen, (0, 0))
                         pygame.display.update()
+
+                self.btnRestart_click(event, timer)
+                self.btnBack_click(event)
+
+    def gameOver(self, text):
+        gameover_font = pygame.font.SysFont("Arial", 60)
+        gameover_font.set_italic(True)
+        gameover_text = gameover_font.render("Game Over, " + text, True, "sandybrown")
+        self.screen.blit(gameover_text, (
+            round(self.grid_size / 2 - gameover_text.get_width() / 2),
+            round(self.grid_size / 2 - gameover_text.get_height() / 2)))
+        self.is_gameOver = True
 
     def play(self, row, col):
         """
@@ -188,11 +273,7 @@ class Chess(object):
         if not self.is_win():
             pass
         else:
-            gameover_font = pygame.font.SysFont("Arial", 60)
-            gameover_text = gameover_font.render("Game Over, " + self.is_win(), True, (255, 0, 0))
-            self.screen.blit(gameover_text, (
-                round(self.grid_size / 2 - gameover_text.get_width() / 2), round(self.grid_size / 2 - gameover_text.get_height() / 2)))
-            # running = False
+            self.gameOver(self.is_win())
 
     def is_win(self):
         """
@@ -224,6 +305,8 @@ class Chess(object):
                 elif r > 3 and c < self.cell_num - 4 and self.board[r][c] == self.board[r - 1][c + 1] == \
                         self.board[r - 2][c + 2] == self.board[r - 3][c + 3] == self.board[r - 4][c + 4] == WHITE:
                     return "White win!"
+
+        return False
 
 if __name__ == '__main__':
     chess = Chess()
